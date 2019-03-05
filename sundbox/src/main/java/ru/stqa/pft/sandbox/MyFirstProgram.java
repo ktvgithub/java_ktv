413144acc12a5fbcb769ca3a193e3bab0d75bd43
Karasova Tatyana <ktvbox@mail.ru>
2019-03-06 00:12:01 +0500

    Создан метод Point и функция distance

--- sundbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sundbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -13,12 +13,19 @@ public class MyFirstProgram {
     Rectangle r = new Rectangle(4, 5);
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
+    Point p = new Point (2,5,3,8);
+    System.out.println("Расстояние между точками" + " = " + distance(p));
+
   }
+
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
 
+  public static double distance(Point p) {
+    return Math.sqrt(Math.sqrt(p.x2-p.x1)+Math.sqrt(p.y2-p.y1));
+  }
 
 }
 
