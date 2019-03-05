3229a5983a3f9b2a941a033fac7defacaf2a0897
Karasova Tatyana <ktvbox@mail.ru>
2019-03-05 21:02:32 +0500

    Выполнено форматирование кода

--- sundbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sundbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,9 +2,9 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-  public static void main(String[] args)  { 
-     System.out.print("Hello, world!");
-}
+  public static void main(String[] args) {
+    System.out.print("Hello, world!");
+  }
 
 }
 
