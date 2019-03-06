b086f367a26e640eaf32d2b823c411bd9ddc7951
Karasova Tatyana <ktvbox@mail.ru>
2019-03-06 22:33:24 +0500

    Выполнена первая часть задания №2

--- sundbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sundbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,28 +3,17 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    hello("wold");
-    hello("user");
-    hello("Tatyana");
+    Point p1 = new Point(7.0, 25.0);
+    Point p2 = new Point(15.0, 30.0);
 
-    Square s = new Square(5);
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
-
-    Rectangle r = new Rectangle(4, 5);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
-
-    Point p = new Point (2,5,3,8);
-    System.out.println("Расстояние между точками" + " = " + distance(p));
+    System.out.println("Расстояние между точками " + "p1("+p1.x+","+p1.y+")" + " и " + "p2("+p2.x+","+p2.y+")" + " = " + distance(p1, p2));
 
   }
 
 
-  public static void hello(String somebody) {
-    System.out.println("Hello, " + somebody + "!");
-  }
+  public static double distance(Point p1, Point p2) {
 
-  public static double distance(Point p) {
-    return Math.sqrt(Math.sqrt(p.x2-p.x1)+Math.sqrt(p.y2-p.y1));
+    return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
   }
 
 }
