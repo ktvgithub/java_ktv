a728bc4e864b401aa699da3f101833a444b3fb0d
Karasova Tatyana <ktvbox@mail.ru>
2019-03-07 21:10:23 +0500

    Выполнено задание №2

--- sundbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sundbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,14 +6,11 @@ public class MyFirstProgram {
     Point p1 = new Point(7.0, 25.0);
     Point p2 = new Point(15.0, 30.0);
 
-    System.out.println("Расстояние между точками " + "p1("+p1.x+","+p1.y+")" + " и " + "p2("+p2.x+","+p2.y+")" + " = " + distance(p1, p2));
+    System.out.println("Расстояние между точками " +
+            "p1(" + p1.x + "," + p1.y + ")" + " и " +
+            "p2(" + p2.x + "," + p2.y + ")" + " = " +
+            p1.distance(p2));
 
-  }
-
-
-  public static double distance(Point p1, Point p2) {
-
-    return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
   }
 
 }
--- sundbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sundbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -8,5 +8,7 @@ public class Point {
     this.x = x;
     this.y = y;
   }
-
+  public double distance(Point p2)  {
+    return Math.sqrt((p2.x - x) * (p2.x - x) + (p2.y - y)
+            * (p2.y - y)); }
 }
