bd886c9ab5fe0aa6c65527355b518b5dbad54861
Karasova Tatyana <ktvbox@mail.ru>
2019-03-20 21:32:36 +0500

    Внесены изменения в программный код по лекции 3.9

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -11,11 +11,17 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void gotoGroupPage() {
-    click(By.linkText("groups"));
+   if (isElementPresent(By.tagName("h1")) && wd.findElement(By.tagName("h1")).getText().equals("Group")
+           &&isElementPresent(By.name("new")))
+    { return;
+    } else {
+    click(By.linkText("groups")); }
   }
 
   public void returnToContactPage() {
-    click(By.linkText("home"));
+    if (isElementPresent(By.id("maintable")))
+    {return;
+    } else {click(By.linkText("home"));}
    }
 
 }
