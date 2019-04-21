4ad46fe134003b6dc05f31955a25df283e5a7476
Karasova Tatyana <ktvbox@mail.ru>
2019-04-21 18:26:11 +0500

    Работает тест LoginTests

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
@@ -34,8 +34,8 @@ public class HttpSession {
       post.setEntity(new UrlEncodedFormEntity(params));
       CloseableHttpResponse response = httpclient.execute(post);
       String body = getTextFrom(response);
-      return body.contains(String.format("<span class=\"italic\">%s</span>", username));
-    }
+      return body.contains(String.format("<span class=\"user-info\">%s</span>", username));
+      }
 
     private String getTextFrom (CloseableHttpResponse response) throws IOException {
       try {
@@ -49,7 +49,7 @@ public class HttpSession {
       HttpGet get = new HttpGet(app.getProperty("web.baseUrl") + "/index.php");
       CloseableHttpResponse response = httpclient.execute(get);
       String body = getTextFrom(response);
-      return body.contains(String.format("<span class=\"italic\">%s</span>", username));
+      return body.contains(String.format("<span class=\"user-info\">%s</span>", username));
     }
   }
 
