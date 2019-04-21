65176375fdffea50d7383617a5b54777226c0c7b
Karasova Tatyana <ktvbox@mail.ru>
2019-04-21 17:10:08 +0500

    реализван тест LoginTest

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/LoginTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/LoginTests.java
@@ -1,17 +1,18 @@
 package ru.stqa.pft.mantis.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.mantis.appmanager.HttpSession;
 
 import java.io.IOException;
+
+import static org.testng.Assert.assertTrue;
 
 public class LoginTests extends TestBase {
 
   @Test
   public void testLogin() throws IOException {
     HttpSession session = app.newSession();
-   Assert.assertTrue(session.login("administrator", "root"));
-   Assert.assertTrue(session.isLoggetInAs("administrator"));
+   assertTrue(session.login("administrator", "root"));
+   assertTrue(session.isLoggetInAs("administrator"));
   }
 }
